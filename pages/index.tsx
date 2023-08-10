74c5e27c40fdd107515753c2987b0edd06baabb5
mert <mertozka41@gmail.com>
2023-08-10 00:41:32 -0400

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { Box, Text } from "@chakra-ui/react";
+function Home() {
+  return (
+    <main>
+      <Text backgroundColor={"red"}>gangang</Text>
+      <Box></Box>
+    </main>
+  );
+}
+
+export default Home;
